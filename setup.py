37e263fe8c9eeb129770b12c570e3132c0347374
Justin Alan Ryan <juryan@ziprealty.com>
2015-11-23 11:29:32 -0800

    adjust to src/, add dep.

--- setup.py
+++ setup.py
@@ -14,10 +14,12 @@ setup(name='bamboo_automate',
       author_email='bitmonk@icloud.com',
       url='',
       license='Apache',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages('src',),
+      package_dir={'':'src'},
       include_package_data=True,
       zip_safe=True,
       install_requires=[
+          'lxml',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
